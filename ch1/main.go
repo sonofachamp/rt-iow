3560b1af200a923e15583fe6b466a2d64d79b923
sonofachamp <fischer.nick1@gmail.com>
2018-05-05 10:25:37 -0500

    playing with ppm format and profiling

--- ch1/main.go
+++ ch1/main.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	memprofile = flag.String("memprofile", "", "write mem profile to file")
 )
 
 func main() {
-	nx := 200
-	ny := 100
+	flag.Parse()
+
+	cpuProfile()
+	width := 200
+	height := 100
 
 	fmt.Println("P3")
-	fmt.Println(nx, ny)
+	fmt.Println(width, height)
 	fmt.Println(255)
 
-	for j := ny - 1; j >= 0; j-- {
-		for i := 0; i < nx; i++ {
-			rpct := float64(i) / float64(nx)
-			gpct := float64(j) / float64(ny)
-			bpct := .2
+	for row := height - 1; row >= 0; row-- {
+		for column := 0; column < width; column++ {
+			redPercent := float64(column) / float64(width)
+			greenPercent := float64(row) / float64(height)
+			bluePercent := .2
 
-			r := int(rpct * 255)
-			g := int(gpct * 255)
-			b := int(bpct * 255)
+			r := int(redPercent * 255)
+			g := int(greenPercent * 255)
+			b := int(bluePercent * 255)
 
 			fmt.Println(r, g, b)
 		}
+	}
+
+	memoryProfile()
+}
+
+func cpuProfile() {
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+}
+
+func memoryProfile() {
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f.Close()
 	}
 }
